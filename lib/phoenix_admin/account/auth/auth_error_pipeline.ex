3fc024d846d468aed6d08c98720a4e7ffa31adc8
virkonomy <arif@tokenomy.com>
2019-12-07 23:24:56 +0700

    fix warning

--- lib/phoenix_admin/account/auth/auth_error_pipeline.ex
+++ lib/phoenix_admin/account/auth/auth_error_pipeline.ex
@@ -1,10 +1,8 @@
 defmodule PhoenixAdmin.Auth.AuthErrorHandler do
   @moduledoc false
 
-  import Plug.Conn
-
-  def auth_error(conn, {type, _reason}, _opts) do
-  	Phoenix.Controller.redirect(conn, to: "/login")
+  def auth_error(conn, {_type, _reason}, _opts) do
+    Phoenix.Controller.redirect(conn, to: "/login")
     # body = Poison.encode!(%{message: to_string(type)})
     # send_resp(conn, 401, body)
   end
